d42f1d9456ae42f3eb8389fcbdb2ac6bed7520f1
Lucas Santos <lucas@aluno.faculdadeimpacta.com.br>
2021-10-03 13:26:51 -0300

    finalizacao do modulo 4 - criacao de token com jwt, uso de autenticacao Stateless no spring security authentication manager.

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +19,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private AutenticacaoService autenticacaoService;
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
+    }
 
     //Configuracoes de autenticacao
     @Override
@@ -33,6 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     http.authorizeRequests()
             .antMatchers(HttpMethod.GET,"/topicos").permitAll()
             .antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
+            .antMatchers(HttpMethod.POST,"/auth").permitAll()
             .anyRequest().authenticated()
             .and().csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
